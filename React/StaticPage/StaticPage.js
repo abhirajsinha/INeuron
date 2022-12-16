e31360c3aa54e39dc4e104c91ff669fc3350da44
Abhiraj <abhirajsinha25@gmail.com>
2022-12-16 08:52:09 +0530

    added contents

--- React/StaticPage/StaticPage.js
+++ React/StaticPage/StaticPage.js
@@ -1,15 +1,34 @@
 function Page() {
   return (
     <div>
-      <img src="./React.png" width="80px"></img>
-      <h1>Fun Factos, that not really a FUN !</h1>
+      <header>
+        <nav>
+          <img src="./React.png" width="80px"></img>
+        </nav>
+      </header>
+      <h1>Fun Facts, that not really a FUN !</h1>
       <ul>
-        <li><strong>Was first released in 2013</strong></li>
-        <li><strong>Was originally created by jordan wake</strong></li>
-        <li><strong>Has well over 100k+ stars on Github</strong></li>
-        <li><strong>Is maintained by facebook</strong></li>
-        <li><strong>Powers thousands of enterprise apps, including mobile apps</strong></li>
+        <li>
+          <strong>Was first released in 2013</strong>
+        </li>
+        <li>
+          <strong>Was originally created by jordan wake</strong>
+        </li>
+        <li>
+          <strong>Has well over 100k+ stars on Github</strong>
+        </li>
+        <li>
+          <strong>Is maintained by facebook</strong>
+        </li>
+        <li>
+          <strong>
+            Powers thousands of enterprise apps, including mobile apps
+          </strong>
+        </li>
       </ul>
+      <footer>
+        <small>2022 Abhiraj development. All right reserved</small>
+      </footer>
     </div>
   );
 }
