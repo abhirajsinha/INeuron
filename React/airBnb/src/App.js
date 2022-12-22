3fb57ddc7659773441c8e0476c6a912829caf3a6
Abhiraj <abhirajsinha25@gmail.com>
2022-12-22 13:05:25 +0530

    Addedd images and rendering locations

--- React/airBnb/src/App.js
+++ React/airBnb/src/App.js
@@ -3,21 +3,31 @@ import Navbar from "./Components/Navbar";
 import "./index";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+import { data } from "./data";
 
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        description={item.description}
+        price={item.price}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        openSpots={item.openSpots}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img="Tommy.png"
-        imgs="Star.png"
-        rating="5.6"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with katie Zaferes"
-        price={136}
-      />
+      <section className="cards--list">{cards}</section>
     </div>
   );
 }
--- React/airBnb/src/Components/Card.jsx
+++ React/airBnb/src/Components/Card.jsx
@@ -3,17 +3,24 @@ import Star from "../images/Star.png"
 
 export default function Card(props){
     console.log(props)
+    let badgeText;
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT";
+    }else if(props.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return(
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={TommyImg} className="card--img"/>
             <div className="card--stats">
                 <img src={Star} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) •</span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>Life Lessons with katy jafears</p>
-            <p><span className="bold">From ${props.price}</span>/ Person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span>from ${props.price}</span>/ Person</p>
         </div>
     )
 }
