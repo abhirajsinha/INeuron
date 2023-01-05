fa7e255276c279dd7426ed44742e20ba63ce393f
Abhiraj <abhirajsinha25@gmail.com>
2023-01-05 11:10:09 +0530

    completed card

--- React/card/src/App.js
+++ React/card/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  //Class Based React
+  const [contact, setContact] = React.useState({
+    firstName: "Abhiraj",
+    lastName: "Sinha",
+    phone: "+91 821 033 7603",
+    email: "abhirajsinha25@gmail.com",
+    isFavorite: false,
+  });
+  /**
+   * Challenge: Fill in the values in the markup
+   * using the properties of our state object above
+   * (Ignore `isFavorite` for now)
+   */
+
+  let starIcon = contact.isFavorite ? "star.png" : "starEmpty.png";
+  function toggleFavorite() {
+    setContact((prevContact) => {
+      return {
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
+      };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className="card">
+        <img src="./images/profile.png" className="card--image" />
+        <div className="card--info">
+          <img
+            src={`./images/${starIcon}`}
+            className="card--favorite"
+            onClick={toggleFavorite}
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
   );
 }
-
-export default App;
