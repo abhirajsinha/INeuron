f2a8054091d6c2090521ea06e358ba550f75830c
Abhiraj <abhirajsinha25@gmail.com>
2023-01-10 11:45:17 +0530

    Change property through setsqaures array

--- React/Challenges/box/src/App.js
+++ React/Challenges/box/src/App.js
@@ -8,7 +8,23 @@ export default function App(props) {
   const [squares, setSquares] = React.useState(boxes);
 
   function toggle(id) {
-    console.log(id);
+    // console.log(id);
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
   }
 
   const squareElements = squares.map((square) => (
--- React/Challenges/box/src/Box.js
+++ React/Challenges/box/src/Box.js
@@ -3,11 +3,14 @@ import React from "react";
 
 export default function Box(props) {
   const [on, setOn] = React.useState(props.on);
-//   function toggle() {
-//     setOn((prevOn) => !prevOn);
-//   }
   const styles = {
-    backgroundColor: on ? "#222222" : "lightblue",
+    backgroundColor: props.on ? "#222222" : "lightblue",
   };
-  return <div style={styles} onClick={()=>props.toggle(props.id)} className="box"></div>;
+  return (
+    <div
+      style={styles}
+      onClick={() => props.toggle(props.id)}
+      className="box"
+    ></div>
+  );
 }
