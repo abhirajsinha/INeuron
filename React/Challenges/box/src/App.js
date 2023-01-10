ef5450bbd1185c806ab197db09d7a35792f1c789
Abhiraj <abhirajsinha25@gmail.com>
2023-01-10 11:30:09 +0530

    Show id when a box clicked

--- React/Challenges/box/src/App.js
+++ React/Challenges/box/src/App.js
@@ -6,9 +6,13 @@ import React from "react";
 
 export default function App(props) {
   const [squares, setSquares] = React.useState(boxes);
-  
+
+  function toggle(id) {
+    console.log(id);
+  }
+
   const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on}/>
+    <Box key={square.id} on={square.on} id={square.id} toggle={toggle} />
   ));
   return <main>{squareElements}</main>;
 }
--- React/Challenges/box/src/Box.js
+++ React/Challenges/box/src/Box.js
@@ -3,11 +3,11 @@ import React from "react";
 
 export default function Box(props) {
   const [on, setOn] = React.useState(props.on);
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
+//   function toggle() {
+//     setOn((prevOn) => !prevOn);
+//   }
   const styles = {
     backgroundColor: on ? "#222222" : "lightblue",
   };
-  return <div style={styles} onClick={toggle} className="box"></div>;
+  return <div style={styles} onClick={()=>props.toggle(props.id)} className="box"></div>;
 }
