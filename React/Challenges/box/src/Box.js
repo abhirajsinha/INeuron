689aaf4eb8175c6adfab24fc4925ed7c4e17fb91
Abhiraj <abhirajsinha25@gmail.com>
2023-01-10 11:15:26 +0530

    toggle the value on to off using onlclick

--- React/Challenges/box/src/Box.js
+++ React/Challenges/box/src/Box.js
@@ -2,9 +2,12 @@ import boxes from "./boxes.js";
 import React from "react";
 
 export default function Box(props) {
-    let color = props.on ? "#222222" : "grey";
-    const styles = {
-        backgroundColor: color,
-    };
-    return <div style={styles} className="box"></div>;
+  const [on, setOn] = React.useState(props.on);
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+  const styles = {
+    backgroundColor: on ? "#222222" : "lightblue",
+  };
+  return <div style={styles} onClick={toggle} className="box"></div>;
 }
