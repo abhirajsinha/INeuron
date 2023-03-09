67be1a77430a8ffc4ebf5a82b8df7a87ebac8137
Abhiraj <abhirajsinha25@gmail.com>
2023-03-09 22:23:04 +0530

    fetching random image urls

--- React/memegenerator/src/Components/Meme.jsx
+++ React/memegenerator/src/Components/Meme.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import memesData from "../memesData";
 
 const Meme = () => {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top Text" className="form--input" />
         <input type="text" placeholder="Bottom Text" className="form--input" />
-        <button className="form--button">Get a new meme Image</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme Image 🗺️
+        </button>
+      </div>
     </main>
   );
 };
