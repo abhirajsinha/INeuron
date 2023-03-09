3b20a71828be0375332c593bf64be9902c60ec76
Abhiraj <abhirajsinha25@gmail.com>
2023-03-09 20:12:39 +0530

    Added meme title

--- React/memegenerator/src/Components/Header.jsx
+++ React/memegenerator/src/Components/Header.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Style.css";
 
 const Header = () => {
   return (
-    <header className='flex items-center ht-[65px] text-white p-[20px] bg-gradient-[(900deg, #672280 1.18%, #A626D3 100%)]'>
-        <img className='header--image' src="/images/TrollFace.png"/>
-        <h2 className='header-title'>Meme Generator</h2>
-        <h4 className=''>React Course - Project 3</h4>
+    <header className="header">
+      <img src="./images/TrollFace.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
