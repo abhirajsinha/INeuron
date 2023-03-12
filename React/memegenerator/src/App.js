43b80de0130529eb18993ea59fe2347b0aa3a96a
Abhiraj <abhirajsinha25@gmail.com>
2023-03-12 15:08:18 +0530

    addded redux folder and state for meme app

--- React/memegenerator/src/App.js
+++ React/memegenerator/src/App.js
@@ -4,21 +4,10 @@ import Header from "./Components/Header";
 import Meme from "./Components/Meme";
 
 function App() {
-  const [thingsArray, setThingsArray] = useState(["t1", "t2", "e", "2", "a"]);
-  //const [thing, setThings] = useState("");
-
-  const thingsElem = thingsArray.map(thing => <p key={thing}>{thing}</p>);
-
-  function addItem() {
-    const lastItem = `Things ${thingsArray.length + 1}`;
-    setThingsArray(prevState => [...prevState, lastItem]);
-  }
   return (
     <>
       <Header />
       <Meme />
-      <button onClick={addItem}> Add Item</button>
-      {thingsElem}
     </>
   );
 }
--- React/memegenerator/src/Components/Meme.jsx
+++ React/memegenerator/src/Components/Meme.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import memesData from "../memesData";
 
 const Meme = () => {
+  const [memeImage, setMemeImage] = React.useState();
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    setMemeImage(memesArray[randomNumber].url);
   }
-
 
   return (
     <main>
@@ -17,7 +18,7 @@ const Meme = () => {
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme Image 🗺️
         </button>
-
+        <img className="meme--image" src={memeImage}/>
       </div>
     </main>
   );
