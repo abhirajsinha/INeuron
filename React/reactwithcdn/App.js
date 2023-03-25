57cf54e9cb5bd109f8d540760e9da04cc4de857f
Abhiraj <abhirajsinha25@gmail.com>
2023-03-25 08:57:03 +0530

    more about react createelement

--- React/reactwithcdn/App.js
+++ React/reactwithcdn/App.js
@@ -17,6 +17,57 @@ React.createElement("The tag you want to create", {all its attributes means id a
 React.createElement("div", {id:"parent"},
 React.createElement("div", {id:"child"},
 React.createElement("h1", {}, "Hello World From React")));
+
+
+# Now what if i need to add 2 Childrens under the <div id="child">
+So for adding more than 1 childrens we can write the same code and push them all in an Array.
+
+<div id="parent">
+    <div id="child">
+        <h1>Hello World From React</h1>
+        <h2>Hello World From React</h2>
+    </div>
+</div>
+
+
+
+React.createElement("div", {id:"parent"},
+React.createElement("div", {id:"child"},
+[
+    React.createElement("h1", {}, "Hello World From React"),
+    React.createElement("h2", {}, "Hello World From React")
+]
+));
+
+
+
+#
+<div id="parent">
+    <div id="child">
+        <h1>Hello World From React</h1>
+        <h2>Hello World From React</h2>
+    </div>
+    <div id="child">
+        <h1>Hello World From React</h1>
+        <h2>Hello World From React</h2>
+    </div>
+</div>
+
+
+React.createElement("div", {id:"parent"},
+[
+    React.createElement("div", {id:"child"},
+    [
+        React.createElement("h1", {}, "Hello World From React"),
+        React.createElement("h2", {}, "Hello World From React")
+    ],
+    React.createElement("div", {id:"child"},
+    [
+        React.createElement("h1", {}, "Hello World From React"),
+        React.createElement("h2", {}, "Hello World From React")
+    ]
+]
+));
 */
 
 console.log(heading); // This heading is not a h1 tag yet, currently it is a object which is a React Element.
