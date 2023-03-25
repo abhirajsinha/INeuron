9cad99fa1a032ed834bf779a5f7c5f4468e434e4
Abhiraj <abhirajsinha25@gmail.com>
2023-03-25 08:45:18 +0530

    more about react

--- React/reactwithcdn/App.js
+++ React/reactwithcdn/App.js
@@ -1,5 +1,25 @@
 const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
-console.log(heading); // This heading is not a h1 tag yet, currently it is a Javascript object where the type is h1
+
+/*
+React.createElement("The tag you want to create", {all its attributes means id and class that the tag will have}, The content that the tag will hold under it);
+*/
+
+/*
+# Make this structure using React.createElement()
+
+<div id="parent">
+    <div id="child">
+        <h1>Hello World From React</h1>
+    </div>
+</div>
+
+
+React.createElement("div", {id:"parent"},
+React.createElement("div", {id:"child"},
+React.createElement("h1", {}, "Hello World From React")));
+*/
+
+console.log(heading); // This heading is not a h1 tag yet, currently it is a object which is a React Element.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Now this root.render will create the h1 tag here from the heading and attach it to the <div id='root'> </div>
